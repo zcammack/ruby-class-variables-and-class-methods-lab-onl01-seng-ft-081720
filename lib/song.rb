43d52fa1c61b7d43d699e4775928164d851b4d2b
Zackary Cammack <zack.cammack@gmail.com>
2020-08-27 16:12:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,10 +11,16 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres << genre
   end
 
   def count
     @@count
   end
 
+  def genres
+    @@genres.uniq!
+  end
+
+  
 end
