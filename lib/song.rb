54a465ebfca4169f92c4b2c977586cba625ef4df
Zackary Cammack <zack.cammack@gmail.com>
2020-08-27 16:31:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,13 +29,14 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    @@genres.each do |hash, a|
+    @@genres.each do |genre| 
       if genre_count[genre]
-        genre_count[genre] += 1
+      genre_count[genre] += 1 
       else
-        genre_count[genre] = 1
+      genre_count[genre] = 1
       end
     end
+    genre_count
   end
 
 end
